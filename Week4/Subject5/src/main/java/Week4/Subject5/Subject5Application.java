7521e9002c704849e20b7dd780edd29263953079
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-01-30 23:20:53 +0900

    [feat] Week5 upload

---
+++ Week4/Subject5/src/main/java/Week4/Subject5/Subject5Application.java
@@ -0,0 +1,13 @@
+package Week4.Subject5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Subject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Subject5Application.class, args);
+	}
+
+}
---
+++ Week4/Subject5/src/test/java/Week4/Subject5/Subject5ApplicationTests.java
@@ -0,0 +1,13 @@
+package Week4.Subject5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Subject5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
